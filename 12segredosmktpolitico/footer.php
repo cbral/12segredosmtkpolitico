036a830c878ace7c5656d1df8183d8b6839ae21d
jr01a <jr01a@icloud.com>
2020-05-25 11:40:18 -0300

    removendo arquivos de produção

--- 12segredosmktpolitico/footer.php
+++
@@ -1,5 +0,0 @@
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.min.js" async defer></script>
-<?php wp_footer(); ?>
-</body>
-
-</html>
--- 12segredosmktpolitico/index.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); ?>
-<h1><?php bloginfo('name'); ?></h1>
-<?php get_footer(); ?>
