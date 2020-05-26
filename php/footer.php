6de91ee1e1907e21f3556918cccaffb4ccafd6b8
jr01a <jr01a@icloud.com>
2020-05-26 01:48:57 -0300

    personalização do banner e seção vídeo explicativo

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,14 @@
+<!-- Modal -->
+<div class="modal fade" id="modalVideo" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/tBacvJMUGKs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/app.min.js" async defer></script>
 <?php wp_footer(); ?>
 </body>
--- php/index.php
+++ php/index.php
@@ -2,10 +2,53 @@
 <div id="bannerTopo" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="<?php echo get_stylesheet_directory_uri(); ?>/images/banner.jpg" alt="Primeiro Slide">
+      <img class="d-block w-100 image-item" src="<?php echo bloginfo('template_directory'); ?>/images/banner.jpg">
       <div class="carousel-caption">
         <div class="container">
-          <div class="row"></div>
+          <div class="row">
+            <div class="col-12">
+              <div class="d-flex justify-content-center">
+                <div>
+                  <div class="box-point d-flex justify-content-between">
+                    <?php $logoInter = 1;
+                    $delay = 200;
+                    while ($logoInter <= 12) : ?>
+                      <div class="point" data-aos="zoom-in-right" data-aos-duration="500" data-aos-delay="<?php echo $delay ?>"></div>
+                    <?php $logoInter++;
+                      $delay = $delay + 200;
+                    endwhile; ?>
+                  </div>
+                  <div class="logo">
+                    <h1 data-aos="fade-right" data-aos-easing="ease" data-aos-delay="400">Os 12 Segredos</h1>
+                    <h1 data-aos="fade-right" data-aos-easing="ease" data-aos-delay="800">do Marketing</h1>
+                    <h1 data-aos="fade-right" data-aos-easing="ease" data-aos-delay="1200">Político De resultados</h1>
+                  </div>
+                  <hr class="divisor" data-aos="zoom-in-right" data-aos-delay="1200">
+                  <h2 class="subtitulo-logo" data-aos="fade-up" data-aos-delay="1500">Método SSI</h2>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <a href="" target="_blank" class="btn-default" data-aos="zoom-in-down" data-aos-delay="1800">quero adquirir agora</a>
+            </div>
+            <div class="col-12" data-aos="fade-up" data-aos-delay="1900">
+              <a href="#videoExplicativo" data-scroll='smoth'>
+                <img class="proxima-secao" src="<?php echo bloginfo('template_directory'); ?>/images/proxima-secao.svg" role="presentation">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="video-explicativo" id="videoExplicativo">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="chamada-modal" data-toggle="modal" data-target="#modalVideo">
+
+          <img src="<?php echo bloginfo('template_directory'); ?>/images/video.jpg" alt="" srcset="<?php echo bloginfo('template_directory'); ?>/images/video.jpg">
         </div>
       </div>
     </div>
